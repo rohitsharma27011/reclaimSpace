b3e72354eeeb83e398caf477c7fb4f9864dcbe58
sloppy-coder <tanmaypani357@gmail.com>
2024-12-29 13:36:09 +0530

    added new pages for findings, requests, and claims, and also made an ItemCard component

---
+++ src/pages/RecievedRequests/RecievedRequests.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ItemCard from '../../components/ItemCard'
+
+const RecievedRequests= ()=> {
+  return (
+    <div>
+      <div className='bg-white'>
+        <ItemCard text="APPROVE REQUEST/REJECT REQUEST"/>
+      </div>
+    </div>
+  )
+}
+
+export default RecievedRequests
---
+++ src/pages/YourClaims/YourClaims.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ItemCard from "../../components/ItemCard";
+const YourClaims = () => {
+  return (
+    <div>
+      <div className="bg-white">
+        <ItemCard text="REQUEST PENDING/ REQUEST APPROVED" />
+      </div>
+    </div>
+  );
+};
+
+export default YourClaims;
